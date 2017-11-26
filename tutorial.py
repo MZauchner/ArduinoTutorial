760ec5d9705a14ea8572d19ad6076539bed317a6
MZauchner <mario.zauchner39@gmx.net>
2017-11-26 19:08:18 +0000

    commit

--- tutorial.py
+++ tutorial.py
@@ -1,10 +1,9 @@
 import serial #Import Serial Library
 
-ser  = serial.Serial('/dev/cu.usbmodem1421',9600) #Create Serial port object called arduinoSerialData
+ser  = serial.Serial('/dev/cu.usbmodem1421',9600) #Create Serial port object
 
 while (1==1):
 
-#    ser.write("123\n".encode("utf-8"))
     myData = ser.readline()
     myData = myData.decode("utf-8")
     print(myData)
